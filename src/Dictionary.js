94454cc872278f1a0f3ce82be51c0025f8eee690
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-07-23 20:11:19 -0500

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,6 +15,7 @@ function handleResponse(response) {
   function search(event) {
     event.preventDefault();
   
+    //documentation : https//dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
