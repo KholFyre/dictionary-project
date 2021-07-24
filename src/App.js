9e3e9036e123140a685eb2b5366d7e9882369a5f
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-07-23 21:36:45 -0500

    Edit spacing in footer

--- src/App.js
+++ src/App.js
@@ -14,14 +14,14 @@ export default function App() {
           This project was coded by &nbsp;
       <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/">
       Rachel Dyer
-      </a> and is &nbsp;
+      </a> and is&nbsp;
       <a
         className="github-link"
         href="https://github.com/KholFyre/dictionary-project"
       >
-        open-sourced on Github and {""}
+        open-sourced on Github{""}
       </a>
-      &nbsp;is hosted on &nbsp;
+      &nbsp;and is hosted on &nbsp;
       <a
         className="netlify-link"
         href="https://mystifying-pike-88124d.netlify.app/"
